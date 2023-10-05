9d2fb496ad9ddc81ae26b8e198bccd01e0ae97a5
gregbouraoui <gregorybouraoui@gmail.com>
2023-10-05 03:18:10 -0400

    idek

--- src/javaScript/main.js
+++ src/javaScript/main.js
@@ -2,6 +2,10 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 
 /*==================== scroll sections active link ====================*/
 let sections = document.querySelectorAll('section');
@@ -23,8 +27,12 @@ window.onscroll = () => {
     });
 
     /*==================== sticky navbar ====================*/
+    let header = document.querySelector('header');
 
+    header.classList.toggle('sticky', window.scrollY > 100);
     /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
 
 
